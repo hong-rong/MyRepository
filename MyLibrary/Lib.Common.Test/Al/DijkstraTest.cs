f84f782fa184d0cc5aca24a38e99cbfe05707f2a
hong-rong <londoner008@gmail.com>
2015-09-11 22:59:26 +0100

    add weight to sample graph

---
+++ MyLibrary/Lib.Common.Test/Al/DijkstraTest.cs
@@ -0,0 +1,25 @@
+﻿using Lib.Common.Al.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Common.Test.Al
+{
+    [TestClass]
+    public class DijkstraTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Dijkstra d = new Dijkstra(DirectedGraph.CreateGraph49());
+
+            d.ShortestPath(0);
+
+            Debug.WriteLine(d.ToString());
+        }
+    }
+}
---
+++ MyLibrary/Lib.Common.Test/Ds/Pq/MinPQTest.cs
@@ -0,0 +1,39 @@
+﻿using Lib.Common.Al.Graph;
+using Lib.Common.Ds.Pq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Common.Test.Ds.Pq
+{
+    [TestClass]
+    public class MinPQTest
+    {
+        private MinPQ<Distance> _target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _target = new MinPQ<Distance>();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var d1 = new Distance { V = 0, Dist = 3 };
+            var d2 = new Distance { V = 0, Dist = 2 };
+            var d3 = new Distance { V = 0, Dist = 1 };
+
+            _target.Insert(d1);
+            _target.Insert(d2);
+            _target.Insert(d3);
+
+            Assert.AreEqual(d3, _target.DelMin());
+            Assert.AreEqual(d2, _target.DelMin());
+            Assert.AreEqual(d1, _target.DelMin());
+        }
+    }
+}
