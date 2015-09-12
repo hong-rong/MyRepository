ed14b15d50b187ba4fb889dbe6c9f22dc1dbe3ca
hong-rong <londoner008@gmail.com>
2015-09-12 15:40:16 +0100

    add new files

---
+++ MyLibrary/Lib.Common/Al/Graph/DagShortestPath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Common.Al.Graph
+{
+    public class DagShortestPath
+    {
+        public static ShortestPathStats ShortestPath(DirectedGraph g, int s)
+        {
+            #region Initialization
+            var stats = new ShortestPathStats(g.V);
+            for (int i = 0; i < stats.Dist.Length; i++)
+            {
+                stats.Dist[i] = int.MaxValue;
+                stats.Prev[i] = -1;
+            }
+            stats.Dist[s] = 0;
+            #endregion
+
+            var dfs = new Dfs(g.V);
+            var dfsStats = dfs.DepthFirstSearch(g);
+            var linearization = dfsStats.Linearization;
+            Debug.WriteLine(dfsStats);
+            foreach (var u in linearization)
+            {
+                foreach (var v in g.Adjacent(u))
+                {
+                    if (stats.Dist[v.V2] > stats.Dist[v.V2] + v.Weight)
+                    {
+                        stats.Dist[v.V2] = stats.Dist[v.V2] + v.Weight;
+                        stats.Prev[v.V2] = u;
+                    }
+                }
+            }
+
+            return stats;
+        }
+    }
+}
