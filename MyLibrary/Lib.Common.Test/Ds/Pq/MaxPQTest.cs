905f5e0e2601a02c82866844ac68b4928c432563
hong-rong <londoner008@gmail.com>
2015-09-16 21:30:59 +0100

    update file

--- MyLibrary/Lib.Common.Test/Ds/Pq/MaxPQTest.cs
+++ MyLibrary/Lib.Common.Test/Ds/Pq/MaxPQTest.cs
@@ -21,14 +21,16 @@ namespace Lib.Common.Test.Ds.Pq
             var d1 = new Distance { V = 0, Dist = 1 };
             var d2 = new Distance { V = 0, Dist = 2 };
             var d3 = new Distance { V = 0, Dist = 3 };
-
+            
             _target.Insert(d1);
             _target.Insert(d2);
             _target.Insert(d3);
 
+            Assert.AreEqual(3, _target.Size());
             Assert.AreEqual(d3, _target.DelRoot());
             Assert.AreEqual(d2, _target.DelRoot());
             Assert.AreEqual(d1, _target.DelRoot());
+            Assert.AreEqual(0, _target.Size());
         }
     }
 }
